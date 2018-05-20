4314b7afd1c12830abcd4988607d106e0834dee0
Victor <email.victor.butko@gmail.com>
2018-05-21 00:52:00 +0300

    update html templates

--- algorithms/bst_non_balancing_add/index.php
+++ algorithms/bst_non_balancing_add/index.php
@@ -1,11 +1,11 @@
 <?php include_once dirname(__FILE__) . '../../../html-templates/header.php'; ?>
 <content-view>
     <view-header>
-        <back-btn><double-arrow-right></double-arrow-right><span>Повернутись</span></back-btn><title-block>Додавання елемента до BST без балансування</title-block>   
+        <back-btn><double-arrow-right>&nbsp;</double-arrow-right><span>Повернутись</span></back-btn><title-block>Додавання елемента до BST без балансування</title-block>   
     </view-header>
     <view-row>
         <data-structure>
-            <title-block><arrow-down></arrow-down><span>Додавання елемента до BST без балансування</span></title-block>
+            <title-block><arrow-down>&nbsp;</arrow-down><span>Додавання елемента до BST без балансування</span></title-block>
             <active-field>
 
             </active-field>
@@ -24,8 +24,9 @@
         </right-block>
     </view-row>
     <view-row>
+        
         <data-pseudocode>
-            <title-block><arrow-down></arrow-down><span>НАЗВА виконуваної процедури</span></title-block>
+            <title-block><arrow-down>&nbsp;</arrow-down><span>НАЗВА виконуваної процедури</span></title-block>
             <active-field>
 
             </active-field>
@@ -34,16 +35,18 @@
             <procedures-block>
                 <title-block>Процедури</title-block>  
                 <active-field>
-
+ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
                 </active-field>  
             </procedures-block>
         </right-block>
     </view-row>
     <view-row>
-        <help-btn><help-sign></help-sign><span>Довідка</span></help-btn>  
-        <controls-block>
-            <start-btn><span>Старт</span><help-sign></help-sign></start-btn>  
-        </controls-block>
+        <help-btn><help-btn-icon>&nbsp;</help-btn-icon><span>Довідка</span></help-btn>  
+        <control-btn class="start"><span>Старт</span><control-icon>&nbsp;</control-icon></control-btn>  
+        <control-btn class="back"><control-icon>&nbsp;</control-icon><span>назад</span></control-btn>  
+        <control-btn class="pause"><span>ПАУЗА</span><control-icon>&nbsp;</control-icon></control-btn>  
+        <control-btn class="forward"><span>вперед</span><control-icon>&nbsp;</control-icon></control-btn> 
+        <control-btn class="stop"><span>СТОП</span><control-icon>&nbsp;</control-icon></control-btn> 
     </view-row>
     <?php
     include_once dirname(__FILE__) . '../../../html-templates/software-info.php';
