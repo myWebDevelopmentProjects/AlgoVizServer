17582263a1beb849decebb6131f9e94a4445deb4
Victor <email.victor.butko@gmail.com>
2018-05-26 15:52:53 +0300

    update html templates

--- algorithms/bst_non_balancing_add/index.php
+++ algorithms/bst_non_balancing_add/index.php
@@ -12,14 +12,41 @@
                 <item class="init">1</item>
             </p>
                 <bst-structure>
-                    <bst-item class="root">R</bst-item >
-                    <bst-item class="_1_1">0</bst-item>
-                    <bst-item class="_1_2">0</bst-item>
+                    <bst-item  id="bst_item_0" class="_0" >0</bst-item>
 
-                    <bst-item class="_2_1">0</bst-item>
-                    <bst-item class="_2_2">0</bst-item>
-                    <bst-item class="_2_3">0</bst-item>
-                    <bst-item class="_2_4">0</bst-item>
+                    <bst-item  id="bst_item_1" class="_1_1" >1</bst-item>
+                    <bst-item  id="bst_item_2" class="_1_2" >2</bst-item>
+
+                    <bst-item  id="bst_item_3" class="_2_1" >3</bst-item>
+                    <bst-item  id="bst_item_4" class="_2_2" >4</bst-item>
+                    <bst-item  id="bst_item_5" class="_2_3" >5</bst-item>
+                    <bst-item  id="bst_item_6" class="_2_4" >6</bst-item>
+
+                    <bst-item  id="bst_item_7" class="_3_1" >7</bst-item>
+                    <bst-item  id="bst_item_8" class="_3_2" >8</bst-item>
+                    <bst-item  id="bst_item_9" class="_3_3" >9</bst-item>
+                    <bst-item  id="bst_item_10" class="_3_4" >10</bst-item>
+                    <bst-item  id="bst_item_11" class="_3_5" >11</bst-item>
+                    <bst-item  id="bst_item_12" class="_3_6" >12</bst-item>
+                    <bst-item  id="bst_item_13" class="_3_7" >13</bst-item>
+                    <bst-item  id="bst_item_14" class="_3_8" >14</bst-item>
+
+                    <bst-item  id="bst_item_15" class="_4_1" >15</bst-item>
+                    <bst-item  id="bst_item_16" class="_4_2" >16</bst-item>
+                    <bst-item  id="bst_item_17" class="_4_3" >17</bst-item>
+                    <bst-item  id="bst_item_18" class="_4_4" >18</bst-item>
+                    <bst-item  id="bst_item_19" class="_4_5" >19</bst-item>
+                    <bst-item  id="bst_item_20" class="_4_6" >20</bst-item>
+                    <bst-item  id="bst_item_21" class="_4_7" >-</bst-item>
+                    <bst-item  id="bst_item_22" class="_4_8" >-</bst-item>
+                    <bst-item  id="bst_item_23" class="_4_9" >-</bst-item>
+                    <bst-item  id="bst_item_24" class="_4_10" >-</bst-item>
+                    <bst-item  id="bst_item_25" class="_4_11" >-</bst-item>
+                    <bst-item  id="bst_item_26" class="_4_12" >-</bst-item>
+                    <bst-item  id="bst_item_27" class="_4_13" >-</bst-item>
+                    <bst-item  id="bst_item_28" class="_4_14" >-</bst-item>
+                    <bst-item  id="bst_item_29" class="_4_15" >-</bst-item>
+                    <bst-item  id="bst_item_30" class="_4_16" >-</bst-item>
 
 
 
